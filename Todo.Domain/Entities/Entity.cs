f9eb420232f3c9860380a92969b68653d64a1088
mathbueeno <mathbueeno@gmail.com>
2022-12-01 13:31:53 -0300

    Criação do Entity Base

--- Todo.Domain/Entities/Entity.cs
+++ Todo.Domain/Entities/Entity.cs
@@ -2,10 +2,19 @@ using System;
 
 namespace Todo.Domain.Entities
 {
-    public abstract class Entity
+    public abstract class Entity : IEquatable<Entity>
     {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
         // Guid é um identificador único
         public Guid Id { get; private set; }
-        
+
+        public bool Equals(Entity other)
+        {
+            return Id == other.Id;
+        }
     }
 }
